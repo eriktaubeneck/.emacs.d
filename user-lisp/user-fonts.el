1e5bab2b4190c1e9488524378d41dc442672fcfe
Erik Taubeneck <erik.taubeneck@gmail.com>
2023-02-27 13:32:21 -0800

    have a list of preferred fonts, pick first, warn when one is not installed

--- user-lisp/user-fonts.el
+++ user-lisp/user-fonts.el
@@ -1,6 +1,43 @@
 ;; Set Font
-(add-to-list 'default-frame-alist '(font . "Berkeley Mono-16" ))
 
+(defvar font-size "16")
+
+(defun font-size-string (font font-size)
+  (concatenate 'string font "-" font-size))
+
+;; list of fonts, in order of preference
+(defvar fonts
+  '(
+    "Berkeley Mono"
+    "Hack"
+    "Menlo"
+    ))
+
+
+(defun find-installed-font (fonts)
+  "Finds the first installed font in FONTS."
+  (catch 'font-found
+    (dolist (font fonts)
+      (if (find-font (font-spec :name font))
+        (progn
+          (throw 'font-found font)
+          )
+        (unless (file-exists-p "~/.emacs.d/.suppress-font-warning")
+          (warn (format "Font %s is not installed. Touch ~/.emacs.d/.suppress-font-warning to ignore"  font))
+          )
+        )
+      )
+    )
+  )
+
+
+(let ((font (find-installed-font fonts)))
+  (set-frame-font (font-size-string font font-size))
+  )
+
+
+
+;; ligatures!
 (autoload 'ligatures "ligatures" nil t)
 
 ;; Enable the www ligature in every possible major mode
