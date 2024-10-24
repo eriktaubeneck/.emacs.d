69767bf6870e3d62ac0e89be97cf98f22a3a12ac
Erik Taubeneck <erik.taubeneck@gmail.com>
2024-10-24 10:19:43 -0700

    update rust mode settings; performance still not great

--- user-lisp/rust-setup.el
+++ user-lisp/rust-setup.el
@@ -1,21 +1,19 @@
 (require 'rust-mode)
-
 (add-hook 'rust-mode-hook
           (lambda () (setq indent-tabs-mode nil)))
-
 (setq rust-format-on-save t)
-
 (define-key rust-mode-map (kbd "C-c C-c") 'rust-run)
-
-(add-hook 'rust-mode-hook 'flycheck-mode)
-
-;; lsp-mode bug
-(add-to-list 'image-types 'svg)
-
+;; Adjust flycheck to check only on save
+(add-hook 'rust-mode-hook (lambda ()
+                            (setq flycheck-check-syntax-automatically '(save mode-enabled))))
+;; lsp-mode configuration
 (require 'lsp-mode)
 (setq lsp-rust-all-features t)
 (setq lsp-rust-server 'rust-analyzer)
+(setq lsp-diagnostics-provider :flycheck)  ; Use flycheck as the diagnostics provider
 (add-hook 'rust-mode-hook #'lsp)
 (setq lsp-log-io t)
 
+;; lsp-mode bug fix related to image types
+(add-to-list 'image-types 'svg)
 (provide 'rust-setup)
